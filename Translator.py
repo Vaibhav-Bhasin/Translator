ef1f557399394426757f7b1233e48ab6fa9c08d9
Vaibhav Bhasin <75664061+Vaibhav-Bhasin@users.noreply.github.com>
2021-10-19 21:40:50 +0530

    Update Translator.py

--- Translator.py
+++ Translator.py
@@ -1,1 +1,27 @@
-
+#### Translator
+eng_fr={'hello':'salut',
+        'very':'tres',
+        'good':'Bon',
+        'see':'voir',
+        'soon':'bientôt',
+        'bye':'au revoir',
+        "house":"maison",
+       "apple":"pomme",
+       "tree":"arbre",
+       "horse":"cheval",
+       "yellow":"jaune",
+       "keyboard":"clavier",
+       "water":"eau",
+       "start":"commencer",
+       "city":"ville",
+       "ear":"oreille",
+       "eel":"anguille",
+       "tooth":"dent",
+      "thank you":"merci",
+      "thank": "remercier",
+      "you":"vous"}
+translate = input("Enter something in English:: ")
+translate = translate.split()
+print("In French we say it:")
+for i in translate:
+  print(eng_fr[i],end = " ")
